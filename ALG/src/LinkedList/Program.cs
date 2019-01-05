998083d85c9d36cd9dfdd537371a6537b3f9fe6f
sunchong <15269607640@163.com>
2019-01-06 07:46:35 +0800

    数组+链表+图谱

---
+++ ALG/src/LinkedList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> links = new LinkedList<string>();
+            LinkedListNode<string> node1 = new LinkedListNode<string>("公共");
+            LinkedListNode<string> node2 = new LinkedListNode<string>("共享");
+            LinkedListNode<string> node3 = new LinkedListNode<string>("测试");
+            LinkedListNode<string> node4 = new LinkedListNode<string>("产品");
+
+            //节点串联
+            links.AddFirst(node1);
+            links.AddAfter(node1, node2);
+            links.AddAfter(node2, node3);
+
+            foreach (var link in links)
+            {
+                Console.WriteLine(link.ToString());
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
--- ALG/src/StackDataStructure/Program.cs
+++ ALG/src/StackDataStructure/Program.cs
@@ -9,49 +9,6 @@ namespace StackDataStructure
         {
             int c = 0;
             int new_c = Interlocked.Increment(ref c);
-
-        }
-
-        void GetProduct()
-        {
-            BaseProduct p;
-            string sourceCode;
-            p = new Product();
-            sourceCode = p.GetCode();
-            sourceCode = Product.GetNewCode(sourceCode);
         }
     }
-
-    public class BaseProduct
-    {
-        public string GetCode()
-        {
-            string maxCode = "001";
-            return maxCode;
-        }
-
-        public virtual string Trim(string source)
-        {
-            return source.Trim();
-        }
-    }
-
-    public class Product : BaseProduct
-    {
-        private static string _type = "PI";
-
-        public override string Trim(string source)
-        {
-            return source.Replace(" ", string.Empty);
-        }
-
-        public static string GetNewCode(string parentCode)
-        {
-            string currentMaxCode = "001001001";
-            string newCode = $"{parentCode}{currentMaxCode}";
-            return newCode;
-
-        }
-    }
-
 }
